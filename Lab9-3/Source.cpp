5e3029072746ae3c9e0b1ca87faede2099f8e453
gboecker24 <g-boecker@onu.edu>
2024-11-05 17:03:29 -0500

    Finished

--- Lab9-3/Source.cpp
+++ Lab9-3/Source.cpp
@@ -9,7 +9,27 @@ bool hasRun(int values[], int size);
 
 int main() {
 
+    const int rolls = 20;
+    int roll_values[rolls];
 
+    srand(time(0));
+
+    cout << endl;
+
+    for (int i = 0; i < rolls; i++) {
+        roll_values[i] = (rand() % 6) + 1;
+    }
+    if (hasRun(roll_values, rolls)) {
+        cout << "has run: ";
+        displayRun(roll_values, rolls);
+    }
+    else {
+        cout << "no run: ";
+        for (int i = 1; i < rolls; i++) {
+            cout << roll_values[i] << " ";
+        }
+    }
+    cout << endl;
 }
 
 void displayRun(int values[], int size) {
