5ca4d2758d4e5fd0f54990afacd17402d724d3c8
gboecker24 <g-boecker@onu.edu>
2024-11-05 16:58:16 -0500

    displayRun Finished

--- Lab9-3/Source.cpp
+++ Lab9-3/Source.cpp
@@ -3,3 +3,20 @@
 #include <iostream>
 
 using namespace std;
+
+void displayRun(int values[], int size);
+bool hasRun(int values[], int size);
+
+int main() {
+
+
+}
+
+void displayRun(int values[], int size) {
+    for (int i = 0; i < size; i++) {
+        if (values[i] == values[i + 1] && values[i] == values[i - 1]) cout << values[i] << " ";
+        else if (values[i] == values[i + 1]) cout << "(" << values[i] << " ";
+        else if (values[i] == values[i - 1]) cout << values[i] << ") ";
+        else cout << values[i] << " ";
+    }
+}
